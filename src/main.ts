c5886c1e7a90c6b72043de01e6d25639ae524adb
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-07-11 11:25:11 +0700

    chore: change prettier config format

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
   const config = new DocumentBuilder()
     .setTitle('Fastify NestJS')
